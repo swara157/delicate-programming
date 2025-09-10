368516979fcdcdf995b683c1ee65490460c8bc36
swara157 <swaravirkud2006@gmail.com>
2025-09-10 18:57:28 +0530

    Add files via upload

---
+++ add_1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a=5;
+    int b=2;
+    int sum;
+    printf("Two numbers are : ");
+    printf("%d   ",a);
+    printf("&   %d",b);
+    sum=a+b;
+    printf("\n The sum of two numbers is %d ", sum);
+    return 0;
+}
