45825ba8e8ea5ae632f5806875854dad4ef8752b
swara157 <swaravirkud2006@gmail.com>
2025-09-11 19:23:02 +0530

    Add files via upload

---
+++ multiply_1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter any two numbers to multiply them  ");
+    scanf("%d",&a );
+    scanf("%d",&b );
+    printf("The two number entered are %d %d\n",a,b);
+     c=a*b;
+    printf("The multiplication of two numbers is %d ", c);
+    return 0;  
+}
