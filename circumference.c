6a99a7c68c560f0459f9b19afff689ef72cc56e1
swara157 <swaravirkud2006@gmail.com>
2025-09-16 16:54:37 +0530

    Add files via upload

---
+++ circumference.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float r,P;
+    printf("enter radius of circle to get circumference ");
+    scanf("%f",&r);
+    P=2*3.14*r;
+    printf("The circumference of the circle is = %f ",P);
+    return 0;
+}
