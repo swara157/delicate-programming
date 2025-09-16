00250f536bfb9e1cf66bb754403f24124df0a43a
swara157 <swaravirkud2006@gmail.com>
2025-09-16 16:30:40 +0530

    Add files via upload

---
+++ square.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int s,A;
+    printf("enter side of square to get area ");
+    scanf("%d",&s);
+    A=s*s;
+    printf("The Area of the square is = %d ",A);
+    return 0;
+}
