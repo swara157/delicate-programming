362fe5ac62f8dbfbfd7754461e0fc7c6fe16736a
swara157 <swaravirkud2006@gmail.com>
2025-09-16 16:38:53 +0530

    Add files via upload

---
+++ sqperimeter.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int s,A;
+    printf("Enter side of square to get perimeter ");
+    scanf("%d",&s);
+    A=4*s;
+    printf("The Perimeter of the square is = %d ",A);
+    return 0;
+}
