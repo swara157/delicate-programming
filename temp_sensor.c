c8c0670d3e832367eab5b5cf1f1d6d38d3199a4d
swara157 <swaravirkud2006@gmail.com>
2025-09-26 06:22:43 +0530

    Add files via upload

---
+++ temp_sensor.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    float sensortemp,threshold=80,critical=100;
+    printf("Enter your body tempreture ",sensortemp);
+    scanf("%f",&sensortemp);
+    if(sensortemp<=threshold)
+    {
+        printf("Patient status : Normal");
+    }
+    else if(sensortemp > threshold && critical > sensortemp)
+    {
+        printf("Patinet status : Warning");
+    }
+    else
+    {
+        printf("Patient status : Critical");
+    }
+    return 0;
+}
