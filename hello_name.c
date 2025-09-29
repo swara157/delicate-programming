f0cfb42c45a82f7ca5b81d34dab361b065af136b
swara157 <swaravirkud2006@gmail.com>
2025-09-29 17:49:15 +0530

    Add files via upload

---
+++ hello_name.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    char a[100];
+    printf("Enter your name : ");
+    scanf("%s",&a);
+    printf("Hello Robotics!\n");
+    printf("Welcome %s ",a);
+    return 0;
+}
