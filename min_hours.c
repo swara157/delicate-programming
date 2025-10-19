e7621fb0318fed4fd5cf724d34e3c39b9da0af7f
swara157 <swaravirkud2006@gmail.com>
2025-10-19 18:31:31 +0530

    Add files via upload

---
+++ min_hours.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float h,m;
+    printf("Enter mintues ");
+    scanf("%f",&m);
+    h=m/60;
+    printf("Hours = %.2f",h);
+    return 0;
+}
---
+++ min_sec.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float s,m;
+    printf("Enter Minutes ");
+    scanf("%f",&m);
+    s=m*60;
+    printf("Seconds = %.2f",s);
+    return 0;
+}
