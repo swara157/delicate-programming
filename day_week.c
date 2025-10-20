cfef98ac1b23aacd0198cf104bead38f7e24fc92
swara157 <swaravirkud2006@gmail.com>
2025-10-20 13:15:27 +0530

    Add files via upload

---
+++ day_week.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float x,y;
+    printf("Enter Days ");
+    scanf("%f",&x);
+    y=x/7;
+    printf("Weeks = %.2f",y);
+    return 0;
+}
---
+++ week_day.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float x,y;
+    printf("Enter Weeks  ");
+    scanf("%f",&x);
+    y=x*7;
+    printf("Days = %.2f",y);
+    return 0;
+}
