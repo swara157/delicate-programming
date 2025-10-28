d5d9984aed392b7aab2cb4f16c05d821729576d1
swara157 <swaravirkud2006@gmail.com>
2025-10-28 21:25:33 +0530

    Add files via upload

---
+++ 24hour.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    float a,b;
+    printf("Enter time to convert in 24 hour format \n");
+    scanf("%f",&a);
+    printf("Enter if its AM=1 or PM=0 \n ");
+    scanf("%d",&n);
+    if(n==1)
+    {
+        printf("Time = %.1f A.M.",a);
+    }
+    else if(n==0)
+    {
+        b=a+12;
+        printf("Time = %.1f P.M.",b);
+    }
+    else
+    {
+        printf("Invalid parameter entered");
+    }
+    return 0;
+}
