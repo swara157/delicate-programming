d45d85b4a454264858795a1bd16ff13ee16990e0
swara157 <swaravirkud2006@gmail.com>
2025-11-04 19:59:49 +0530

    Add files via upload

---
+++ variable.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    int a=34;
+    float b = 99.9;
+    char c = 'O';
+    printf("Roll no. = %d\n",a);
+    printf("Percent = %.2f\n",b);
+    printf("Grade = %c",c);
+}
