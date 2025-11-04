9bc3523cb20664938e6549e385d47bb2b66579ba
swara157 <swaravirkud2006@gmail.com>
2025-11-04 19:58:58 +0530

    Add files via upload

---
+++ euros_dollar.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float r,d;
+    printf("Enter euros to know in dollar ");
+    scanf("%f",&r);
+    d=r*1.149;
+    printf("Dollar = %.3f",d);
+    return 0;
+}
