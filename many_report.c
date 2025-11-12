326c761d087263f4d007279f78730225670ff6e0
swara157 <swaravirkud2006@gmail.com>
2025-11-12 17:59:44 +0530

    Add files via upload

---
+++ many_report.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+struct human_temp
+{
+    float temperature;
+    int age;
+    char blood[4];
+    char mobile[10];
+};
+int main()
+{
+    struct human_temp h[3];
+    int i;
+    for(i=0;i<1;i++)
+    {
+        printf("Enter Details of person\n");
+        printf("Enter Temperature \n");
+        scanf("%f",&h[i].temperature);
+        printf("Enter Age \n");
+        scanf("%d",&h[i].age);
+        printf("Enter Blood group \n");
+        scanf("%s",&h[i].blood);
+        printf("Enter Mobile \n");
+        scanf("%s",&h[i].mobile);
+    }
+    for(i=0;i<1;i++)
+    {
+        printf("\nDetails of person  :  %d\n\n",i+1);
+        printf("Temperature : %.2f\n",h[i].temperature);
+        printf("Age : %d\n",h[i].age);
+        printf("Blood group : %s\n",h[i].blood);
+        printf("Mobile : %s\n",h[i].mobile);
+    }
+
+}
